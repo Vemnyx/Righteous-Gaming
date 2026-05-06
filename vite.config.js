b2db59c8875c1d313d6264dbd98da7b5fe163c85
Vemnyx <programmerjake95@gmail.com>
2026-05-06 09:50:12 -0400

    added proxy for hitting backend

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+      "/complete-registration": "http://localhost:8080",
+      "/send-email": "http://localhost:8080",
+    },
+  },
 });
