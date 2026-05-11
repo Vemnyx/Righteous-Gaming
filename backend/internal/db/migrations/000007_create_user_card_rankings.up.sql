c618bc38ccb5a7c358e3f63114c9ce1e5488cf4d
Vemnyx <programmerjake95@gmail.com>
2026-05-11 02:26:10 -0400

    set id on user card rankings

--- backend/internal/db/migrations/000007_create_user_card_rankings.up.sql
+++ backend/internal/db/migrations/000007_create_user_card_rankings.up.sql
@@ -1,12 +1,15 @@
 CREATE TABLE user_card_rankings (
     user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    set_id integer NOT NULL REFERENCES sets (id) ON DELETE CASCADE,
     card_id integer NOT NULL REFERENCES cards (id) ON DELETE CASCADE,
     format smallint NOT NULL,
     rank smallint NOT NULL,
     notes varchar(2048),
-    PRIMARY KEY (user_id, card_id, format)
+    PRIMARY KEY (user_id, set_id, card_id, format)
 );
 
 CREATE INDEX user_card_rankings_user_id_idx ON user_card_rankings (user_id);
+CREATE INDEX user_card_rankings_set_id_idx ON user_card_rankings (set_id);
 CREATE INDEX user_card_rankings_card_id_idx ON user_card_rankings (card_id);
 CREATE INDEX user_card_rankings_format_idx ON user_card_rankings (format);
+CREATE INDEX user_card_rankings_user_set_format_idx ON user_card_rankings (user_id, set_id, format);
